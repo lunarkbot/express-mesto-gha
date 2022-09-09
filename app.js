2b9ef6e0da4ff42e20615b4af387022bc71cb2f3
Sergej Bopp <58397321+z-style-org@users.noreply.github.com>
2022-09-09 10:12:59 +0300

    feat: add controller, model, route for users

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '631ae6a4e08ad0c2f524d2b6'
+  };
+
+  next();
+});
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
+
+app.use('/users', require('./routes/users'));
 
 app.listen(PORT);
