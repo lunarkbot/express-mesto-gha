b0c4a770e403656289ddf34233b10c2bda268782
Sergej Bopp <58397321+z-style-org@users.noreply.github.com>
2022-09-09 11:07:24 +0300

    feat: add error handling for users

--- controllers/users.js
+++ controllers/users.js
@@ -3,13 +3,25 @@ const User = require('../models/user');
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then(users => res.send({ data: users }))
-    .catch(err => res.status(500).send({ message: err.message }));
+    .catch(err => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({message: 'Переданы некорректные данные.'});
+        return;
+      }
+      res.status(500).send({ message: err.message })
+    });
 };
 
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.userId)
     .then(user => res.send({data: user}))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(err => {
+      if (err.name === 'CastError') {
+        res.status(404).send({message: `Пользователь с ID ${err.value} не найден.`});
+        return;
+      }
+      res.status(500).send({ message: err.message })
+    });
 }
 
 module.exports.createUser = (req, res) => {
@@ -17,5 +29,11 @@ module.exports.createUser = (req, res) => {
 
   User.create({name, about, avatar})
     .then(user => res.send({_id: user._id, name, about, avatar }))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(err => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({message: 'Переданы некорректные данные.'});
+        return;
+      }
+      res.status(500).send({ message: err.message })
+    });
 }
