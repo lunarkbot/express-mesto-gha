ae72f54849b7c7447d1dae118b5445ae47951b73
lunarkbot <58397321+lunarkbot@users.noreply.github.com>
2022-09-15 00:07:15 +0500

    feat: add new validation rules

--- app.js
+++ app.js
@@ -20,16 +20,16 @@ app.listen(PORT);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required(),
+    email: Joi.string().email().required(),
     password: Joi.string().required().min(8),
   }),
 }), login);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    name: Joi.string(),
-    about: Joi.string(),
-    avatar: Joi.string(),
-    email: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().uri(),
+    email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
 }), createUser);
--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ const {
 router.get('/', getUsers);
 router.get('/:userId',celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().alphanum().required(),
+    userId: Joi.string().alphanum().length(24).required(),
   }),
 }), getUserById);
 router.get('/me', getCurrentUser);
