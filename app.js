ea8f19695c78c09bbc81a273f7c27313370e4bab
lunarkbot <58397321+lunarkbot@users.noreply.github.com>
2022-09-14 21:37:34 +0500

    refactor: remove hardcode

--- app.js
+++ app.js
@@ -10,14 +10,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '631ae6a4e08ad0c2f524d2b6',
-  };
-
-  next();
-});
-
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
