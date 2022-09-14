dc99ed4fba68f5fb2cde634e3e97a02d5b28645b
lunarkbot <58397321+lunarkbot@users.noreply.github.com>
2022-09-15 01:53:11 +0500

    fix regEx pattern

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri().pattern(patternUrl),
+    avatar: Joi.string().uri().regex(patternUrl),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
